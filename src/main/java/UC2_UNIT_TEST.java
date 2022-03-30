88585ff1a0f0dfda9ff266dd5108d457e456c40d
Xavier Hill Roy <xavierhillroy@gmail.com>
2022-03-30 10:33:31 -0400

    Created ValueInvestingStrategy : just another strategy to use for my unit testing Creating a unit test for UC2 ( process of adding and editing broker to broker list). See UC2_UNIT_TEST its not finished yet (numbers on the side represent the step of the process its on) Now just updating the UC2 Test cvase

--- src/main/java/UC2_UNIT_TEST.java
+++ src/main/java/UC2_UNIT_TEST.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class UC2_UNIT_TEST {
     /*
     * 1) CREATE A USER SELECTION OBJECT
@@ -21,9 +23,43 @@ public class UC2_UNIT_TEST {
 // 3)
         Broker fidelity = brokerFactory.create(usrSelect1); // CREATING A BROKER
         Broker charlesSchwab = brokerFactory.create(usrSelect2); // second broker
+
+        // NOW I NEED TO TEST IF EVERYTHING PROPERLY CREATED INCLUDING STRATEGY OBJECT
+        // TESTING CONTENT OF FIDELITY
+        Strategy stratFidelity  = fidelity.getStrategy();
+        String[] tckrLstF = fidelity.getCryptoTickerList();
+
+
+        // FIDELITY BROKER RES
+        System.out.println("FIDELITY RESULTS");
+        String nameF = fidelity.getName();
+        System.out.println(stratFidelity.getClass());
+        System.out.println(tckrLstF[0]);// I FUCKING SWEAR IF I GET A NULL POINTER IM GONNA FLIP
+        System.out.println(tckrLstF[1]);
+
+        // TESTING CHARLES SCHWAB
+
+        Strategy stratCharles  = charlesSchwab.getStrategy();
+        String[] tckrLstCharles = charlesSchwab.getCryptoTickerList();
+        String nameCharles = charlesSchwab.getName();
+
+        System.out.println("CHARLES RESULTS");
+        System.out.println(stratCharles.getClass());
+        System.out.println(tckrLstCharles[0]);
+        System.out.println(tckrLstCharles[1]);
+
+
+
+
+
 // 4)
-        brokerList.addBroker(fidelity); // ADDING BROKER TO LIST
-        brokerList.addBroker(charlesSchwab);
+//        brokerList.addBroker(fidelity); // ADDING BROKER TO LIST
+//        brokerList.addBroker(charlesSchwab);
+//        // TEST TO SEE IF SUCCESFULLY ADDED
+
+
+
+
 
 
 
