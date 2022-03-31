b5a0cf5fa71756cde561f947fbfdfa54530efdeb
Nearzy12 <Samnear17@gmail.com>
2022-03-31 14:58:16 -0400

    Added class MainSystem - just testing GitHub if this will push to repo

---
+++ src/main/java/MainSystem.java
@@ -0,0 +1,10 @@
+public class MainSystem {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------");
+        System.out.println("Main Sytem test print statement");
+        System.out.println("-------------------------------");
+        System.out.println("Testing GitHub file upload");
+        System.out.println("-------------------------------");
+
+    }
+}
