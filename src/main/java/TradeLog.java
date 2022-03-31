aa854324086dc21adcdd075ca7ccffe19cde1a5c
Xavier Hill Roy <xavierhillroy@gmail.com>
2022-03-31 09:14:16 -0400

    Observer design pattern done (from my component) need the viewer method implementatiobn from displaySystem component to complete this design pattern

--- src/main/java/TradeLog.java
+++ src/main/java/TradeLog.java
@@ -3,8 +3,19 @@ import java.util.ArrayList;
 * */
 
 public class TradeLog {
+    // DONT THINK I SHOULD BE CREATING OBJECTS HERE NOT SURE BUT FOR FUNCTIONALITY FOR NOW WILL DO
+    private  HistogramViewer graph;
+    private TableViewer tableViewer;
 
-    private ArrayList<StrategyResult> trades;
+    private ArrayList<Observer> observers = new ArrayList<>();
+    private ArrayList<StrategyResult> trades = new ArrayList<>();
+    TradeLog(){
+        observers.add(graph);
+        observers.add(tableViewer);
+
+
+
+    }
 
     public ArrayList<StrategyResult> getTrades() {
         return trades;
@@ -15,6 +26,11 @@ public class TradeLog {
             if (trade.getDate().equals(date) && trade.getBroker().getName().equals(broker))return trade;
         }
         return null;
+    }
+    public void notifyObservers(){
+        for (Observer o : observers){
+            o.update();
+        }
     }
 
 }
