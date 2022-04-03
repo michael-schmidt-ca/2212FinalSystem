b39eb1e91ffd062f51646b8cba53d16e912be236
Xavier Hill Roy <xavierhillroy@gmail.com>
2022-04-03 09:50:32 -0400

    Created method in table viewer that converts the strategy results to a 2d array for processing

--- src/main/java/TableViewer.java
+++ src/main/java/TableViewer.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class TableViewer implements Observer{
     /*Table viewer = Observer 2 (observer design pattern)*/
@@ -43,8 +45,50 @@ public class TableViewer implements Observer{
 
 
         scrollPane.setPreferredSize(new Dimension(800, 300));
-        table.setFillsViewportHeight(true);;
+        table.setFillsViewportHeight(true);
 
         MainUI.getInstance().updateStats(scrollPane);
+    }
+    private Object[][] toArray(TradeLog tradeLog){
+        // Create a 2d array to store data in size of rows is num of trades, and each trade has 7 attributes ie the columns
+        Object[][] realData = new Object[tradeLog.getTrades().size()][7];
+        ArrayList<StrategyResult> trades = tradeLog.getTrades(); // Container holding actuall trades
+        for (int row =0; row<trades.size(); row++){ // cycling through trades
+            for (int column = 0; column < 7; column++){
+                switch (column){
+                    case 0: {
+                        realData[row][column] = trades.get(row).getBroker().getName(); // puts name into first col of each row
+                        break;
+                    }
+                    case 1: {
+                        realData[row][column] = trades.get(row).getStrategy().getName();
+                        break;
+                    }
+                    case 2:{
+                        realData[row][column]=trades.get(row).getCoin();
+                        break;
+                    }
+                    case 3:
+                    {
+                        realData[row][column]=trades.get(row).getAction();
+                        break;
+                    }
+                    case 4:{
+                        realData[row][column]=trades.get(row).getQuantity();
+                        break;
+                    }
+                    case 5:{
+                        realData[row][column]=trades.get(row).getPrice();
+                        break;
+                    }
+                    case 6:{
+                        realData[row][column]=trades.get(row).getDate();
+                        break;
+                    }
+
+                }// ENd of switch
+            }// end of column for loop
+        }// end of row loop
+    return realData;
     }
 }
