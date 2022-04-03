e83cd21b9f508b83defb8b892728e53ce5835e44
Xavier Hill Roy <xavierhillroy@gmail.com>
2022-04-03 08:35:22 -0400

    Parser completed but needs testing

--- src/main/java/TickerParser.java
+++ src/main/java/TickerParser.java
@@ -1,10 +1,16 @@
 public class TickerParser { // THIS CLASS I IN CHARGE OF CONVERTING TICKER TO ACTUAL NAME SO coingheko can make a call
-
+    // Parser that acts as an intermediate between mikes and i code
     public String getCoinName(String ticker){
         switch (ticker){
             case "BTC": return "bitcoin";
             case "ETH": return "ethereum";
-
+            case "USDT": return "tether";
+            case "BNB": return "BNB";
+            case "SOL": return "solana";
+            default:{
+                System.out.println(ticker+" : Not supported");
+                return null;
+            }
         }
     }
 }
