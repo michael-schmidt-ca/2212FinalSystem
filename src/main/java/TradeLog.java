6808bb88c3ff92137ecb6a42d2e3c6301d707063
Michael Schmidt <michaelschmidt02@outlook.com>
2022-04-04 22:08:57 -0400

    additional comments and bug fixes

--- src/main/java/TradeLog.java
+++ src/main/java/TradeLog.java
@@ -1,7 +1,7 @@
 /**
  * @author Xavier Hill Roy
  * CS2212 - Intro to Software Engineering
- * @purpose This class acts as a container for the the Strategy results and implements the Observer design pattern. This class  acts as a subject
+ * @purpose This class acts as a container for the Strategy results and implements the Observer design pattern. This class  acts as a subject
  */
 import java.util.ArrayList;
 import java.util.Observer;
@@ -9,12 +9,24 @@ import java.util.Observer;
 * */
 
 public class TradeLog implements DisplayInfo{
+
     private ArrayList<TradingObservers> observers = new ArrayList<>();
     private ArrayList<StrategyResult> trades = new ArrayList<>();
 
+    /**
+     * getter method for list of trades
+     * @return arrayList of trade objects (strategyResults)
+     */
     public ArrayList<StrategyResult> getTradeLog() {
         return trades;
     }
+
+    /**
+     * getter method for a trade object
+     * @param broker broker who completed the trade
+     * @param date date the trade was completed
+     * @return a trade that matches the param
+     */
     public StrategyResult getTrade(String broker, String date){
 
         for (StrategyResult trade: trades){
@@ -23,14 +35,28 @@ public class TradeLog implements DisplayInfo{
         return null;
     }
 
+    /**
+     * method to trigger observer functions related to trades and trade completion
+     */
     public void notifyObservers(){
         for (TradingObservers o : observers){
             o.update();
         }
     }
+
+    /**
+     * method to insert a trade to the log
+     * @param s the trade object to insert
+     * @return whether the addition was succesful
+     */
     public boolean addTrade(StrategyResult s){
         return trades.add(s);
     }
+
+    /**
+     * method to insert an additional observer function
+     * @param observer the observer to add
+     */
     public void attach(TradingObservers observer){
         observers.add(observer);
     }
